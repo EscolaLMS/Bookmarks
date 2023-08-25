cefbfb4b05d3526f93c96c6807f01584138b4729
Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
2023-08-25 12:11:05 +0200

    Add course title to topic resource

--- src/Http/Resources/TopicResource.php
+++ src/Http/Resources/TopicResource.php
@@ -8,12 +8,15 @@ class TopicResource extends JsonResource
 {
     public function toArray($request): array
     {
+        $course = $this->lesson?->course;
+
         return [
             'id' => $this->id,
             'title' => $this->title,
             'type' => $this->topicable_type,
             'lesson_id' => $this->lesson?->getKey(),
-            'course_id' => $this->lesson?->course?->getKey()
+            'course_id' => $course?->getKey(),
+            'course_title' => $course?->title,
         ];
     }
 }
